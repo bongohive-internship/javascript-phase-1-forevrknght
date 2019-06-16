49a4816bdf7d3b9b67e1ba9c291ce6ea5631fbfb
John N0body <31930326+forevrknght@users.noreply.github.com>
2019-06-16 03:00:40 +0300

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,14 @@
 // you solution should be here
+var dob = 1992;
+var fyear = 3031;
+
+var age = fyear - dob;
+
+var cAge = 26;
+var maxAge = 86;
+var eat = 5;
+
+var ageDif = maxAge - cAge;
+var totalEaten = ageDif * eat;
+
+window.alert("You will need NN to last you until the ripe old age of X");
